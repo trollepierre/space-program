c24780d4ebe4da9c63287242d47c1f2766b8910d
Pierre Recontact <commit@recontact.me>
2022-09-12 20:03:54 +0200

    chore(styles): Add minimum styles to see results responsively

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,20 @@
-import './App.css'
 import React from 'react'
 import { SatellitesPositions } from './modules/SatellitesPositions'
+import styled from 'styled-components'
+
+const StyledApp = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  color: white;
+`
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <SatellitesPositions />
-      </header>
-    </div>
+    <StyledApp>
+      <SatellitesPositions/>
+    </StyledApp>
   )
 }
 
--- front/src/modules/SatellitesPositions.js
+++ front/src/modules/SatellitesPositions.js
@@ -1,5 +1,28 @@
 import React, { useCallback, useState } from 'react'
 import { SatellitePosition } from './SatellitePosition'
+import styled from 'styled-components'
+
+const StyledButton = styled.button`
+  text-align: center;
+  font-size: 24px;
+  border-radius: 8px;
+  padding: 8px;
+`
+const StyledNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 32px;
+  height: 20vh;
+`
+
+const StyledMain = styled.main`
+  height: 30vh;
+  font-size: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 export const SatellitesPositions = () => {
   const [satellitesPositions, setSatellitesPositions] = useState([])
@@ -11,8 +34,11 @@ export const SatellitesPositions = () => {
   }, [])
 
   return <>
-    <button onClick={writeBackSatellitesPositions}>Write back satellites positions</button>
-    {satellitesPositions.map(position => <SatellitePosition key={position.satlatitude} position={position}/>)}
+    <StyledNav>
+      <StyledButton onClick={writeBackSatellitesPositions}>Write back satellites positions</StyledButton>
+    </StyledNav>
+    <StyledMain>
+      {satellitesPositions.map(position => <SatellitePosition key={position.satlatitude} position={position}/>)}
+    </StyledMain>
   </>
-
 }
